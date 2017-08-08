2f993c92024dea6d99275825df9fc4a989264f38
QLGQ <519688681@qq.com>
2017-08-08 16:40:49 +0800

    ADD

--- bin/logic/func/Mail_func.py
+++ bin/logic/func/Mail_func.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
 # !-*- coding:utf-8 -*-
+import smtplib
 from bin.until import Logger
 
 L = Logger.getInstance()
 
-
-
-
 class Mail_func(object):
+    def __init__(self, data):
+        self.from_address = data['from_address']
+        self.password = data['password']
+        self.to_address = data['to_address']
+        self.title = data['title']
+        self.content = data['content']
+        self.type =data['type']
+
     def send_mail(self, data):
+        if data['to_address'] is None:
+            print("not address to send")
+        else:
+            self.to_address = data['to_address']
         try:
-            pass
-        except Exception as e:
-            L.error("send_mail error , msg :%s", e)
-
-        print("发送邮件")
-        pass
-
+            srv = smtplib.SMTP(**srv_kwargs)
+            srv.login(self.from_address, self.password)
+            srv.sendmail(self.from_address, self.to_address, self.content)
+            srv.quit()
+            L.info('send email to user successfully')
+        except smtplib.SMTPException as e:
+            L.warn('send email to user failed')
+            L.exception(e)
 
 def getInstance():
     return Mail_func()
+
+
+def main():
+    Mail_func()
+
+if __name__ == "__main__":
+    main()
